f88b02e08dbe83ce2557d8e5d254cc06aeecc09e
Eliel <elielpdutra.dev@gmail.com>
2023-01-07 21:56:18 -0300

    iniciando em Condições

---
+++ aulas-javascript/condicoes-pt01/ex008.js
@@ -0,0 +1,10 @@
+let vel = 60.5;
+
+console.log(`A velociadade do seu carro é ${vel} km/h.`);
+
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+}
+console.log('Dirija sempre usando o cinto de segurança!');
+
+
