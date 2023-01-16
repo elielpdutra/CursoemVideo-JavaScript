10a86930bf596ae0068bbc2b79986b4a2d5364f4
Eliel <elielpdutra.dev@gmail.com>
2023-01-15 22:12:13 -0300

    iniciando em repetições

---
+++ aulas-javascript/repeticoes/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 3);
+
+
+
+/*
+var c = 1
+
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+
+
+/*
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+*/
